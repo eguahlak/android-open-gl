52c07f17d1d7815d814fcb778b8ca098733385fb
Anders Kalhauge <anders@kalhauge.dk>
2015-12-15 14:25:35 +0100

    Nu med kant :-)

--- app/src/main/java/dk/cphbusiness/exampleopengl/CanvasView.java
+++ app/src/main/java/dk/cphbusiness/exampleopengl/CanvasView.java
@@ -8,6 +8,7 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class CanvasView extends GLSurfaceView implements GLSurfaceView.Renderer {
+    private Square square;
 
     public CanvasView(Context context) {
         super(context);
@@ -19,6 +20,7 @@ public class CanvasView extends GLSurfaceView implements GLSurfaceView.Renderer 
     @Override
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
         GLES20.glClearColor(0f, 0f, 0.5f, 1f);
+        square = new Square();
         }
 
     @Override
@@ -29,6 +31,22 @@ public class CanvasView extends GLSurfaceView implements GLSurfaceView.Renderer 
     @Override
     public void onDrawFrame(GL10 unused) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        square.draw();
+        }
+
+    private static int loadShader(int type, String code) {
+        int shaderHandle = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shaderHandle, code);
+        GLES20.glCompileShader(shaderHandle);
+        return  shaderHandle;
+        }
+
+    public static int loadVertexShader(String code) {
+        return loadShader(GLES20.GL_VERTEX_SHADER, code);
+        }
+
+    public static int loadFragmentShader(String code) {
+        return loadShader(GLES20.GL_FRAGMENT_SHADER, code);
         }
 
     }
---
+++ app/src/main/java/dk/cphbusiness/exampleopengl/Square.java
@@ -0,0 +1,84 @@
+package dk.cphbusiness.exampleopengl;
+
+import android.opengl.GLES20;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+public class Square {
+    private FloatBuffer vertexBuffer;
+    private ShortBuffer drawListBuffer;
+
+    static final int COORDINATES_PER_VERTEX = 3;
+    static float[] coordinates = {
+        -0.5f,  0.5f, 0f,
+        -0.5f, -0.5f, 0f,
+         0.5f, -0.5f, 0f,
+         0.5f,  0.5f, 0f
+        };
+    private short[] drawOrder = {
+        0, 1, 2,
+        0, 2, 3
+        };
+
+    float[] color = { 1f, 1f, 0f, 1f };
+
+    private final int programHandle;
+
+    public Square() {
+        ByteBuffer coordinateBytes = ByteBuffer.allocateDirect(4*coordinates.length);
+        coordinateBytes.order(ByteOrder.nativeOrder());
+        vertexBuffer = coordinateBytes.asFloatBuffer();
+        vertexBuffer.put(coordinates).position(0);
+
+        ByteBuffer drawBytes = ByteBuffer.allocateDirect(2*drawOrder.length);
+        drawBytes.order(ByteOrder.nativeOrder());
+        drawListBuffer = drawBytes.asShortBuffer();
+        drawListBuffer.put(drawOrder).position(0);
+
+        int vertexShaderHandle = CanvasView.loadVertexShader(vertexShaderCode);
+        int fragmentShaderHandle = CanvasView.loadFragmentShader(fragmentShaderCode);
+
+        programHandle = GLES20.glCreateProgram();
+        GLES20.glAttachShader(programHandle, vertexShaderHandle);
+        GLES20.glAttachShader(programHandle, fragmentShaderHandle);
+        GLES20.glLinkProgram(programHandle);
+        }
+
+    private int positionHandle;
+    private int colorHandle;
+
+    private final int vertexCount = coordinates.length/COORDINATES_PER_VERTEX;
+    private final int vertexStride = 4*COORDINATES_PER_VERTEX;
+
+    public void draw() {
+        GLES20.glUseProgram(programHandle);
+        positionHandle = GLES20.glGetAttribLocation(programHandle, "vPosition");
+        GLES20.glEnableVertexAttribArray(positionHandle);
+        GLES20.glVertexAttribPointer(positionHandle, COORDINATES_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
+
+        colorHandle = GLES20.glGetUniformLocation(programHandle, "vColor");
+        GLES20.glUniform4fv(colorHandle, 1, color, 0);
+
+        //GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length, GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+
+        GLES20.glDisableVertexAttribArray(positionHandle);
+        }
+
+    private final String vertexShaderCode =
+            "attribute vec4 vPosition;"+
+            "void main() {"+
+            "  gl_Position = vPosition;"+
+            "  }";
+
+    private final String fragmentShaderCode =
+            "precision mediump float;"+
+            "uniform vec4 vColor;"+
+            "void main() {"+
+            "  gl_FragColor = vColor;"+
+            "  }";
+
+    }
